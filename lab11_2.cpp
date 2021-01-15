c2027b110649758fd4f1abf35e8c32158bcbd6a5
kittiphum <kittiphum_s@cmu.ac.th>
2021-01-15 15:09:07 +0700

    เสร็จ

--- lab11_2.cpp
+++ lab11_2.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int,int);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int x,int y){
+    int result;
+    if(x%y == 0){
+        return y;
+    }
+    else{
+        result = gcd(y,x%y);
+        return result;
+    }
+
+    
 }
