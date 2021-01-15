c45a3af74be48bd800c60fce262555e657437024
kittiphum <kittiphum_s@cmu.ac.th>
2021-01-15 14:17:55 +0700

    เสร็จ

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int x){
+    if(x == 1 or x == 0){
+        return x;
+    }
+    else{
+        x = fibonacci(x-1)+fibonacci(x-2);
+        return x;
+    }
 }
