5f089111f389fd850c98ae87955331f6ad82512d
kittiphum <kittiphum_s@cmu.ac.th>
2021-01-15 14:45:20 +0700

    fibonacci(50)

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,17 +1,15 @@
 #include<iostream>
 using namespace std;
 
-int fibonacci(int);
+long long int fibonacci(long long int);
 
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
+    cout << fibonacci(50);
     return 0; 
 }
 
-int fibonacci(int x){
+long long int fibonacci(long long int x){
     if(x == 1 or x == 0){
         return x;
     }
